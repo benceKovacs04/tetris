a23606830a4f61257998ddfef3dcb7565045905d
Bence <kovacs.ben.p@gmail.com>
2019-09-29 21:11:47 +0200

    can spawn 1 box on the grid

--- src/com/codecool/tetris/Main.java
+++ src/com/codecool/tetris/Main.java
@@ -1,12 +1,10 @@
 package com.codecool.tetris;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
 
 public class Main extends Application {
 
@@ -15,9 +13,15 @@ public class Main extends Application {
 
         Game game = new Game();
 
+        Rectangle box = new Rectangle(25,25);
+        box.setFill(Color.BLACK);
+
+        //game.getChildren().add(box);
+        game.add(box, 4, 0);
+
+
         primaryStage.setScene(new Scene(game, 800, 600));
         primaryStage.show();
-
 
     }
 
