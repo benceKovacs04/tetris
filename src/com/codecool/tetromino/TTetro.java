35dba8badcc054fc186936bd59ed76c282d384c8
Bence <kovacs.ben.p@gmail.com>
2019-10-01 14:38:24 +0200

    TTetro transfomrs

--- src/com/codecool/tetromino/TTetro.java
+++ src/com/codecool/tetromino/TTetro.java
@@ -3,9 +3,12 @@ package com.codecool.tetromino;
 import com.codecool.tetris.TetrominoHandler;
 import javafx.scene.paint.Color;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class TTetro extends Tetromino {
+
+    private int state = 1;
 
     public TTetro(TetrominoHandler game) {
         super(game);
@@ -14,12 +17,12 @@ public class TTetro extends Tetromino {
         pieceOne.setRowNum(1);
 
         pieceTwo.setColNum(5);
-        pieceTwo.setRowNum(1);
+        pieceTwo.setRowNum(0);
 
-        pieceThree.setColNum(5);
-        pieceThree.setRowNum(0);
+        pieceThree.setColNum(6);
+        pieceThree.setRowNum(1);
 
-        pieceFour.setColNum(6);
+        pieceFour.setColNum(5);
         pieceFour.setRowNum(1);
 
         pieceOne.setFill(Color.PURPLE);
@@ -28,9 +31,60 @@ public class TTetro extends Tetromino {
         pieceFour.setFill(Color.PURPLE);
     }
 
+    private boolean middlePieceInBounds() {
+        return pieceFour.getRowNum() < ROWNUMBER - 1 && pieceFour.getRowNum() > 0 &&
+                pieceFour.getColNum() <= COLNUMBER - 2 && pieceFour.getColNum() > 0;
+    }
+
+    private void transformFirstTwoPiece() {
+        pieceOne.setColNum(pieceTwo.getColNum());
+        pieceOne.setRowNum(pieceTwo.getRowNum());
+
+        pieceTwo.setColNum(pieceThree.getColNum());
+        pieceTwo.setRowNum(pieceThree.getRowNum());
+    }
+
     @Override
     public void transform() {
-        //TODO: implement it
+        switch (state) {
+            case 1:
+                if (checkForValidTransform(Map.of("thirdPiece", Arrays.asList(pieceThree.getRowNum() + 1, pieceThree.getColNum() -1)))
+                        && middlePieceInBounds()) {
+                    transformFirstTwoPiece();
+                    pieceThree.setRowNum(pieceThree.getRowNum() + 1);
+                    pieceThree.setColNum(pieceThree.getColNum() - 1);
+                    state = 2;
+                }
+                break;
+            case 2:
+                if (checkForValidTransform(Map.of("thirdPiece", Arrays.asList(pieceThree.getRowNum() - 1, pieceThree.getColNum() - 1)))
+                        && middlePieceInBounds()) {
+                    transformFirstTwoPiece();
+                    pieceThree.setRowNum(pieceThree.getRowNum() - 1);
+                    pieceThree.setColNum(pieceThree.getColNum() - 1);
+                    state = 3;
+                }
+                break;
+            case 3:
+                if (checkForValidTransform(Map.of("thirdPiece", Arrays.asList(pieceThree.getRowNum() - 1 , pieceThree.getColNum() + 1)))
+                        && middlePieceInBounds()
+                ) {
+                    transformFirstTwoPiece();
+                    pieceThree.setRowNum(pieceThree.getRowNum() - 1);
+                    pieceThree.setColNum(pieceThree.getColNum() + 1);
+                    state = 4;
+                }
+                break;
+            case 4:
+                if (checkForValidTransform(Map.of("thirdPiece", Arrays.asList(pieceThree.getRowNum() + 1 , pieceThree.getColNum() + 1)))
+                        && middlePieceInBounds()) {
+                    transformFirstTwoPiece();
+                    pieceThree.setRowNum(pieceThree.getRowNum() + 1);
+                    pieceThree.setColNum(pieceThree.getColNum() + 1);
+                    state = 1;
+                }
+                break;
+        }
     }
 }
 
