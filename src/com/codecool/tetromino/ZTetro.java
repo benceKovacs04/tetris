e5643309ac51561d6a4dac1bdb374b3ea7feaab0
Bence <kovacs.ben.p@gmail.com>
2019-10-01 18:40:53 +0200

    z tetro can transform too

--- src/com/codecool/tetromino/ZTetro.java
+++ src/com/codecool/tetromino/ZTetro.java
@@ -3,9 +3,12 @@ package com.codecool.tetromino;
 import com.codecool.tetris.TetrominoHandler;
 import javafx.scene.paint.Color;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class ZTetro extends Tetromino {
+public class ZTetro extends CenterPieceTetromino {
 
     public ZTetro(TetrominoHandler game) {
         super(game);
@@ -16,21 +19,59 @@ public class ZTetro extends Tetromino {
         pieceTwo.setColNum(5);
         pieceTwo.setRowNum(0);
 
-        pieceThree.setColNum(5);
+        pieceThree.setColNum(6);
         pieceThree.setRowNum(1);
 
-        pieceFour.setColNum(6);
+        pieceFour.setColNum(5);
         pieceFour.setRowNum(1);
 
         pieceOne.setFill(Color.RED);
         pieceTwo.setFill(Color.RED);
         pieceThree.setFill(Color.RED);
-        pieceFour.setFill(Color.RED
-        );
+        pieceFour.setFill(Color.RED);
     }
 
     @Override
-    public void transform() {
-        //TODO: implement
+    protected Map<TetrominoPiece, List<Integer>> getStateOneTransformationMap() {
+        Map<TetrominoPiece, List<Integer>> nextState = new HashMap<>(Map.of(
+                pieceOne, Arrays.asList(pieceOne.getRowNum(), pieceOne.getColNum() + 2),
+                pieceTwo, Arrays.asList(pieceTwo.getRowNum() + 1, pieceTwo.getColNum() + 1),
+                pieceThree, Arrays.asList(pieceThree.getRowNum() + 1 , pieceThree.getColNum() - 1)
+        ));
+
+        return nextState;
+    }
+
+    @Override
+    protected Map<TetrominoPiece, List<Integer>> getStateTwoTransformationMap() {
+        Map<TetrominoPiece, List<Integer>> nextState = new HashMap<>(Map.of(
+                pieceOne, Arrays.asList(pieceOne.getRowNum() + 2, pieceOne.getColNum()),
+                pieceTwo, Arrays.asList(pieceTwo.getRowNum() + 1, pieceTwo.getColNum() - 1),
+                pieceThree, Arrays.asList(pieceThree.getRowNum() - 1, pieceThree.getColNum() - 1)
+        ));
+
+        return nextState;
+    }
+
+    @Override
+    protected Map<TetrominoPiece, List<Integer>> getStateThreeTransformationMap() {
+        Map<TetrominoPiece, List<Integer>> nextState = new HashMap<>(Map.of(
+                pieceOne, Arrays.asList(pieceOne.getRowNum(), pieceOne.getColNum() - 2),
+                pieceTwo, Arrays.asList(pieceTwo.getRowNum() - 1, pieceTwo.getColNum() - 1),
+                pieceThree, Arrays.asList(pieceThree.getRowNum() - 1, pieceThree.getColNum() + 1)
+        ));
+
+        return nextState;
+    }
+
+    @Override
+    protected Map<TetrominoPiece, List<Integer>> getStateFourTransformationMap() {
+        Map<TetrominoPiece, List<Integer>> nextState = new HashMap<>(Map.of(
+                pieceOne, Arrays.asList(pieceOne.getRowNum() - 2, pieceOne.getColNum()),
+                pieceTwo, Arrays.asList(pieceTwo.getRowNum() - 1, pieceTwo.getColNum() + 1),
+                pieceThree, Arrays.asList(pieceThree.getRowNum() + 1, pieceThree.getColNum() + 1)
+        ));
+
+        return nextState;
     }
 }
