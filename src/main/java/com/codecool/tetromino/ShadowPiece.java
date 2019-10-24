d529adc6d535553dd064e401542182ccb5fa267a
Bence Kovács <kovacs.ben.p@gmail.com>
2019-10-24 13:29:27 +0200

    shadow is now transparent

--- src/main/java/com/codecool/tetromino/ShadowPiece.java
+++ src/main/java/com/codecool/tetromino/ShadowPiece.java
@@ -1,4 +1,9 @@
 package com.codecool.tetromino;
 
+import javafx.scene.paint.Color;
+
 public class ShadowPiece extends TetrominoPiece {
+    public ShadowPiece() {
+        setFill(Color.TRANSPARENT);
+    }
 }
